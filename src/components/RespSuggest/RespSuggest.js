7a448926b8358a7cd63ee55b203ff1266fb5caf3
Munchic <khoi.u.pham@gmail.com>
2018-03-24 06:11:34 -0700

    Added msg bubbles

---
+++ src/components/RespSuggest/RespSuggest.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './RespSuggest.css'
+
+const respSuggest = (props) => {
+  return (
+    <div className="RespSuggest">
+      { props.messages.map() }
+    </div>
+  )
+}
+
+export default resSuggest
